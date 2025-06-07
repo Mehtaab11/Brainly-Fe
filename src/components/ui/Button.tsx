1dfd9df6520b481d87f1bc117706c73c5941c819
Mehtaab <mehtaabaalam117@gmail.com>
2025-06-07 19:49:51 +0530

    first commit

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,18 @@
+import type { ReactElement } from "react";
+
+interface buttonProps {
+  variant: "primary" | "secondary";
+  size?: "md" | "lg" | "sm";
+  endIcon?: ReactElement;
+  startIcon?: ReactElement;
+  text: string;
+  onClick: () => void;
+}
+
+const Button = (props: buttonProps) => {
+  return <button></button>;
+};
+
+export default Button;
+
+{/* <Button variant="primary" />; */}
