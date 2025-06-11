4df2cc41b0fa44562ea60b9ab35994e6519785fc
Mehtaab <mehtaabaalam117@gmail.com>
2025-06-11 14:47:40 +0530

    Implemented routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/Signin";
+import { BrowserRouter , Routes , Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      {" "}
-      {/* <Dashboard /> */}
-      <SignIn />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
