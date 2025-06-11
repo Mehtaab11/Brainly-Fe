ce9f0ca3ce01a3e834591a505a77c050f4ea8fae
Mehtaab <mehtaabaalam117@gmail.com>
2025-06-11 14:49:08 +0530

    Add Signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/Signin";
-import { BrowserRouter , Routes , Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignIn />} />
-        <Route path="/" element={<SignIn />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
